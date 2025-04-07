c950e792482d67700581b303c956f318f903e915
jBoscW <jbd393@nyu.edu>
2025-04-07 11:07:55 -0300

    Update main() function

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(str): 
+    return len(str.split())
+
+
 def main():
     path = "books/frankenstein.txt"  # Adjust this path if your file is elsewhere
     text = get_book_text(path)
-    print(text)
+    num_words = count_words(text)
+    print(f'{num_words} words found in the document')
 
-main()
+if __name__ == '__main__':
+    main()
+
